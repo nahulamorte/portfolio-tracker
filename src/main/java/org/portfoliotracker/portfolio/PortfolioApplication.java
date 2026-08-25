a0667e0132a9603100c5dfd202b9177300b2505d
nahulamorte <nahuellamorte@gmail.com>
2026-08-25 15:02:16 -0300

    chore: configuration docker, database and connections

--- src/main/java/org/portfoliotracker/portfolio/PortfolioApplication.java
+++ src/main/java/org/portfoliotracker/portfolio/PortfolioApplication.java
@@ -1,13 +1,22 @@
 package org.portfoliotracker.portfolio;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PortfolioApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
